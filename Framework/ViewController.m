81a6e2f20e9b789ed731d292d3a12ac7827e281c
tim_黄昆 <812559044@qq.com>
2017-09-29 14:38:20 +0800

    mvc

--- Framework/ViewController.m
+++ Framework/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import "MVCController.h"
+#import "MVPController.h"
 @interface ViewController ()
 
 @end
@@ -16,10 +17,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self showMVP];
+    });
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)showMVC
+{
+    MVCController *mvc=[MVCController new];
+    [self presentViewController:mvc animated:true completion:nil];
+    
+}
 
-
+-(void)showMVP
+{
+    MVPController *mvc=[MVPController new];
+    [self presentViewController:mvc animated:true completion:nil];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
